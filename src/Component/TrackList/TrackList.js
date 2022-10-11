81fad86c6815635f10f3ba07c4e5168bc9d39d32
vivinvinh212 <83176944+vivinvinh212@users.noreply.github.com>
2022-10-11 19:39:30 +0700

    added track

--- src/Component/TrackList/TrackList.js
+++ src/Component/TrackList/TrackList.js
@@ -1,11 +1,16 @@
 import React from 'react'
+import Track from '../Track/Track';
 import './TrackList'
 
 class TrackList extends React.Component {
     render() {
         return (
             <div className="TrackList">
-                {/* <!-- You will add a map method that renders a set of Track components  --> */}
+                {
+                    this.props.tracks.map(track => (
+                        <Track key={track.id} name={track.name} artist={track.artist} album={track.album} />
+                    ))
+                }
             </div>
         )
     }
